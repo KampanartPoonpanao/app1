87ad1b145a943e9ca76fa90463f31b46cdb7c0ad
KampanartPoonpanao <110509937+KampanartPoonpanao@users.noreply.github.com>
2023-08-04 15:51:13 +0700

    add style.css update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './style.css'
 
 function App() {
+  const divStyle = {
+    color:'red',
+    backgroundColor:'powderblue',
+    frontSize:'larger',
+    padding:'20px',
+    textAlign:'center'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <h3><center>Hello world! <br/>สวัสดีชาวโลก</center></h3>
+  <div style={divStyle}>Hello React</div>
+  <h2>DeveloperThai.com</h2>
+  <div className='title'>React & React Native</div>
+  </>
   );
 }
 
