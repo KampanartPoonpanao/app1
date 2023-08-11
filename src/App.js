f543440495bd9efed9f7a5c7dfdcab619df2cf03
KampanartPoonpanao <110509937+KampanartPoonpanao@users.noreply.github.com>
2023-08-11 15:07:49 +0700

    Create FuncCon
    create funcCon

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 import React from 'react'
-import './style.css'
+import { Header, Content, Footer } from './func-components'
 
 function App() {
-  const divStyle = {
-    color:'red',
-    backgroundColor:'powderblue',
-    frontSize:'larger',
-    padding:'20px',
-    textAlign:'center'
-  }
+  
   return (
-    <>
-  <h3><center>Hello world! <br/>สวัสดีชาวโลก</center></h3>
-  <div style={divStyle}>Hello React</div>
-  <h2>DeveloperThai.com</h2>
-  <div className='title'>React & React Native</div>
-  </>
-  );
+    [<Header/>,<Content/>,<Footer/>]
+  )
 }
-
-export default App;
+export default App
