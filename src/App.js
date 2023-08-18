6c3e5e0b8414922348ef64990fcdf55db506020c
KampanartPoonpanao <110509937+KampanartPoonpanao@users.noreply.github.com>
2023-08-18 15:25:34 +0700

    add calculator & eventData

--- src/App.js
+++ src/App.js
@@ -1,32 +1,14 @@
 import React from 'react'
-import './style.css'
-import {Header, Content, Footer} from './func-components'
-import Calendar from './class-components' 
 import Banner from './banner'
 import {Calculator} from './calculator'
-
-
+import {EventData2} from './event-data'
 
 function App() {
-  const divStyle = {
-    color:'red',
-    backgroundColor:'powderblue',
-    frontSize:'larger',
-    padding:'20px',
-    textAlign:'center'
-  }
   return (
-    <>
-  <h3><center>Hello world! <br/>สวัสดีชาวโลก</center></h3>
-  <div style={divStyle}>Hello react</div>
-  <h1>DeveloperThai.com</h1>
-  <div className='title'>React & React Native</div>
-  <Header/>
-  <p><center><Calendar/></center></p>
-  <Content/>
-  <Footer/>
+  <>
   <Banner/>
   <Calculator/>
+  <EventData2/> 
   </>
   );
 }
