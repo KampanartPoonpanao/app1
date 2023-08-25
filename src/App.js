1a2ffd6e5002960435c2e4cc4a904b839a7e7326
KampanartPoonpanao <110509937+KampanartPoonpanao@users.noreply.github.com>
2023-08-25 15:42:14 +0700

    create state-func.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import MessageBox from './state-func'
 import RefsFunc from './refs-func'
+import RefsArray from './refs-array'
 
 export default function App() {
-  return <RefsFunc/>
+  return <>
+  <p style={{textAlign:'center'}}>งาน 1 </p><RefsFunc/><hr></hr>
+  <p style={{textAlign:'center'}}>งาน 2 </p><RefsArray/><hr></hr>
+  <p style={{textAlign:'center'}}>งาน 3 </p><MessageBox/><hr></hr>
+  </>
 }
