99af224fd7abcb8f8d74b616c74fa7d5dcaadf6c
KampanartPoonpanao <110509937+KampanartPoonpanao@users.noreply.github.com>
2023-09-30 01:30:19 +0700

    add fromcheck text

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import Router3 from './router3'
+import FormCheck from './form-check'
 
 
 
 export default function App() {
-    return <Router3/>
+    return <FormCheck/>
   
 }
